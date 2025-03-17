839bf2366c38b0fdf583b45e197984d4adadf866
Yashas-naidu <yashashsn555@gmail.com>
2025-03-17 21:44:37 +0530

    changed commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, I am changed\n";
+  cout << "Hello, I am changed Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
